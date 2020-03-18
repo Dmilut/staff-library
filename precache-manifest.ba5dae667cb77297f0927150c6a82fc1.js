62b6967e5136d228b5aaa837e7775d9462f5721a
[Dmilut] <[d.lutcenko2017@gmail.com]>
2020-03-18 14:42:30 -0700

    Updates

---
+++ precache-manifest.ba5dae667cb77297f0927150c6a82fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b575e7e5bf63c778ebfb2f4b436c3e1b",
+    "url": "/staff-library/index.html"
+  },
+  {
+    "revision": "9e397d235189f2c5b332",
+    "url": "/staff-library/static/css/main.b5ce3139.chunk.css"
+  },
+  {
+    "revision": "055e4bab2c5610f51a9b",
+    "url": "/staff-library/static/js/2.9af9c249.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/staff-library/static/js/2.9af9c249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e397d235189f2c5b332",
+    "url": "/staff-library/static/js/main.6e406b95.chunk.js"
+  },
+  {
+    "revision": "7f670c783ae3e4cd79bb",
+    "url": "/staff-library/static/js/runtime-main.fb3d301f.js"
+  }
+]);
